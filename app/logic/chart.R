c6cd75e685e63b7eb499c54a1a3b76b07e4d044b
Jakub Nowicki <kuba@appsilon.com>
2022-08-23 17:52:51 +0200

    js

--- app/logic/chart.R
+++ app/logic/chart.R
@@ -1,6 +1,7 @@
 box::use(
   echarts4r,
-  dplyr
+  dplyr,
+  htmlwidgets[JS],
 )
 
 #' @export
@@ -9,6 +10,6 @@ chart <- function(data) {
     dplyr$group_by(Species) |>
     echarts4r$e_chart(Year) |>
     echarts4r$e_line(Population) |>
-    echarts4r$e_x_axis(Year) |>
+    echarts4r$e_x_axis(Year, formatter = JS("App.formatYear")) |>
     echarts4r$e_tooltip()
 }
